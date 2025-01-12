a0aa096c8f19b3a8d4b619b5bd40e1a5342a6a51
Franco Perez <56205122+dev-lang@users.noreply.github.com>
2025-01-12 12:56:26 -0300

    Create union_all_query_mysql.sql
    query para consulta en más de una base de datos

---
+++ union_all_query_mysql.sql
@@ -0,0 +1,32 @@
+#SELECT * from database_1.personas where nombres like '%Juan%';
+SELECT * FROM database_1.personas WHERE nombres LIKE '%Juan%'
+UNION ALL
+SELECT * FROM database_2.personas WHERE nombres LIKE '%Juan%'
+UNION ALL
+SELECT * FROM database_3.personas WHERE nombres LIKE '%Juan%'
+UNION ALL
+SELECT * FROM database_4.personas WHERE nombres LIKE '%Juan%'
+UNION ALL
+SELECT * FROM database_5.personas WHERE nombres LIKE '%Juan%'
+UNION ALL
+SELECT * FROM database_6.personas WHERE nombres LIKE '%Juan%'
+UNION ALL
+SELECT * FROM database_7.personas WHERE nombres LIKE '%Juan%'
+UNION ALL
+SELECT * FROM database_8.personas WHERE nombres LIKE '%Juan%'
+UNION ALL
+SELECT * FROM database_9.personas WHERE nombres LIKE '%Juan%'
+UNION ALL
+SELECT * FROM database_10.personas WHERE nombres LIKE '%Juan%'
+UNION ALL
+SELECT * FROM database_11.personas WHERE nombres LIKE '%Juan%'
+UNION ALL
+SELECT * FROM database_12.personas WHERE nombres LIKE '%Juan%'
+UNION ALL
+SELECT * FROM database_13.personas WHERE nombres LIKE '%Juan%'
+UNION ALL
+SELECT * FROM database_14.personas WHERE nombres LIKE '%Juan%'
+UNION ALL
+SELECT * FROM database_15.personas WHERE nombres LIKE '%Juan%'
+UNION ALL
+SELECT * FROM database_16.personas WHERE nombres LIKE '%Juan%';
